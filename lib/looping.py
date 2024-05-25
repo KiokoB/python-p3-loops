57bf6879fff2e2a0f26b9a5077cea90afd33568a
Bianca Kioko <kiokobianca01@gmail.com>
2024-05-25 03:00:26 +0300

    worked on loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,33 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 11
+    while i>1:
+        i-= 1
+        print(i)
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squares = [num ** 2 for num in int_list ]
+    return squares
+square_integers([1,2,3,4,5,-1,-2,-3,-4,-5])
+#square_integers([-1,-2,-3,-4,-5])
 
 def fizzbuzz():
     # code goes here!
-    pass
+    b = 0
+    while b < 100:
+        b += 1
+        def fb_num(b):
+            if b % 3==0 and b% 5 == 0:
+                return "FizzBuzz"
+            elif b % 5 == 0:
+                return "Buzz"
+            elif b % 3 == 0:
+                return "Fizz"
+            else:
+                return b
+        print(fb_num(b))
+        
+fizzbuzz()   
